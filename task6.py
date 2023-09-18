ffcb6835f2a151962bf4a72f39688f49590fc5dd
ANKAM ARUNRAJ <95906920+20R01A04J3@users.noreply.github.com>
2023-09-18 10:18:39 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,31 @@
+x=2
+y=5
+print (x+y)
+for x in range(5):
+  print(x)
+
+if x<5:
+  print("x is smaller than 5")
+else:
+  print("x is greater than 5") 
+
+# in python this is a comment line
+
+a=2 #integer
+b=2.0 #float
+c="Arun" #string
+
+
+#strings
+d= str(b)
+print(d)
+
+print(len(c))
+print(c.lower())
+print(c.upper())
+
+# arguments
+def arun(name):
+  print("i'm "+name)
+
+arun("Aunraj")
